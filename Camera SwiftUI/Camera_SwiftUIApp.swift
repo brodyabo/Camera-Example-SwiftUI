cfc9d01448f0f609ddb197b2baf5f52da6ae0b0a
brodyabo <brody.a24@k12.xrds.org>
2023-03-17 09:52:47 -0700

    Initial Commit

---
+++ Camera SwiftUI/Camera_SwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  Camera_SwiftUIApp.swift
+//  Camera SwiftUI
+//
+//  Created by Brody Abowitz on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Camera_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
